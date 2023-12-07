195139959c18dc21fc2d3827aa746792caac5f0f
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-12-07 07:09:44 +0100

    chore: update store.ts

--- store/store.ts
+++ store/store.ts
@@ -3,5 +3,5 @@ import { configureStore } from "@reduxjs/toolkit";
 import CartReducer from "./cartSlice.ts";
 
 export const store = configureStore({
-  //   reducer: CartReducer,
+  reducer: CartReducer,
 });
