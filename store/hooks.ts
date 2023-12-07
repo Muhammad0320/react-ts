dee7b966f2f084041f0ddfab0481aab6cc38c6cf
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2023-12-07 09:38:35 +0100

    chore: add hooks

--- store/hooks.ts
+++ store/hooks.ts
@@ -1,6 +1,12 @@
-import { useDispatch } from "react-redux";
-import { CartDispatch } from "./store";
+import {
+  type TypedUseSelectorHook,
+  useDispatch,
+  useSelector,
+} from "react-redux";
+import { CartDispatch, RootState } from "./store";
 
 type DispatchFunction = () => CartDispatch;
+
+export const useCartSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 export const useCartDispatch: DispatchFunction = useDispatch;
--- store/store.ts
+++ store/store.ts
@@ -8,4 +8,6 @@ export const store = configureStore({
   },
 });
 
+export type RootState = ReturnType<typeof store.getState>;
+
 export type CartDispatch = typeof store.dispatch;
